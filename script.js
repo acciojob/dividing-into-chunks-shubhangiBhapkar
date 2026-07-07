ef7a045941b1543ce59ae94646ee9966d0a60aba
Shubhangi Bhapkar <shubhangibhapkar35@gmail.com>
2026-07-07 12:24:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,26 @@
 const arr = [1, 2, 3, 4, 1, 0, 2, 2];
-
 const divide = (arr, n) => {
-  // Write your code here
+  const result = [];
+  let current = [];
+  let sum = 0;
+
+  for (const num of arr) {
+    if (sum + num <= n) {
+      current.push(num);
+      sum += num;
+    } else {
+      result.push(current);
+      current = [num];
+      sum = num;
+    }
+  }
+
+  if (current.length) {
+    result.push(current);
+  }
+
+  return result;
 };
 
-const n = prompt("Enter n: ");
+const n = 5; // or use prompt("Enter n: ") if running in a browser
 alert(JSON.stringify(divide(arr, n)));
